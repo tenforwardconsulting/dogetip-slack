c2d82d9984be8817f8d635b8893440786a759ca5
Carlos Cardona <cgcardona@gmail.com>
2014-04-26 09:12:08 -1000

    Tweak doge wealthy

--- coin_config/dogecoin.rb
+++ coin_config/dogecoin.rb
@@ -2,7 +2,7 @@ module Dogecoin
   PERFORM_ERROR = 'such error no command wow no amaze'
   BALANCE_REPLY_PRETEXT = 'such balance'
   CURRENCY_ICON = 'Ð'
-  WEALTHY_UPPER_BOUND = 1000
+  WEALTHY_UPPER_BOUND = 100000
   WEALTHY_UPPER_BOUND_POSTTEXT = ' very wealth!'
   WEALTHY_UPPER_BOUND_ICON = ':moneybag:'
   BALANCE_REPLY_POSTTEXT = ' many coin'
