2f989de7d6e6a7471a9522ae3ec99ce43834a50d
Carlos Cardona <cgcardona@gmail.com>
2014-04-26 13:41:07 -1000

    Add zetacoin.

---
+++ coin_config/zetacoin.rb
@@ -0,0 +1,24 @@
+module Dogecoin
+  PERFORM_ERROR = 'such error no command wow no amaze'
+  BALANCE_REPLY_PRETEXT = 'such balance'
+  CURRENCY_ICON = 'Zeta'
+  WEALTHY_UPPER_BOUND = 1000
+  WEALTHY_UPPER_BOUND_POSTTEXT = ' very wealth!'
+  WEALTHY_UPPER_BOUND_EMOJI = ':moneybag:'
+  BALANCE_REPLY_POSTTEXT = ' many coin'
+  DEPOSIT_PRETEXT = 'so deposit'
+  DEPOSIT_POSTTEXT = 'many address wow'
+  TIP_ERROR_TEXT = 'pls say tip @username amount'
+  TIP_PRETEXT = 'such generous!'
+  TIP_POSTTEXT1 = 'http://dogechain.info/tx/'
+  TIP_POSTTEXT2 = '|such blockchain'
+  WITHDRAW_TEXT = 'such stingy'
+  WITHDRAW_ICON = ':shit:'
+  NETWORKINFO_ICON = ':bar_chart:'
+  TOO_POOR_TEXT = 'so poor not money many sorry'
+  NO_PURPOSE_LOWER_BOUND_TEXT = 'such stupid no purpose'
+  NO_PURPOSE_LOWER_BOUND = 1
+  RANDOMIZED_EMOJI = ':black_joker:'
+  NETWORK = 'zetacoin'
+  PORT = 17333
+end
