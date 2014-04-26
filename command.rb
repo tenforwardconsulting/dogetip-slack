729a435de99ae145934cc9a7d778c0ba43911de6
Carlos Cardona <cgcardona@gmail.com>
2014-04-26 11:43:54 -1000

    Capitalize and not upcase.

--- command.rb
+++ command.rb
@@ -27,7 +27,7 @@ class Command
   end
 
   def balance
-    puts "#{ENV['COIN'].upcase::BALANCE_REPLY_PRETEXT}"
+    puts "#{ENV['COIN'].capitalize::BALANCE_REPLY_PRETEXT}"
     balance = client.getbalance(@user_id)
     @result[:text] = "@#{@user_name} #{Dogecoin::BALANCE_REPLY_PRETEXT} #{balance}#{Dogecoin::CURRENCY_ICON}"
     if balance > Dogecoin::WEALTHY_UPPER_BOUND
